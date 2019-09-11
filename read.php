cb98923bfa69985e05f5cc35320b8df4a040b1bb
Cheste1994 <49307381+Cheste1994@users.noreply.github.com>
2019-09-11 15:07:23 +0600

    Update read.php

--- read.php
+++ read.php
@@ -1,6 +1,6 @@
 <?php
     $file = fopen ("Assets\Scripts\file_1.txt", "r");
-	echo fread ($file, filesize(Assets\Scripts\file_1.txt))
+	echo fread ($file, filesize(file_1.txt))
 	fclose($file);
 	}
 ?>
